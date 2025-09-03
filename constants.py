6ea8cafa8a449ed7e01090b030e08d98d15a497d
b4llu97 <147453384+b4llu97@users.noreply.github.com>
2025-09-03 18:13:01 +0200

    Add basic game framework

---
+++ constants.py
@@ -0,0 +1,21 @@
+"""Game-wide constants.
+"""
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Frames per second
+FPS = 60
+
+# Colors (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Gameplay constants
+PLAYER_SPEED = 5
+ENEMY_SPEED = 3
